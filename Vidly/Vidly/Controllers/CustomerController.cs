d6b3b0f555ac6a9ec1e973f718315174f86df744
Jain, Himanshu <himanshu_jain10@optum.com>
2019-03-04 12:33:37 +0530

    Duplicate Vidly
    Duplicate Vidly

---
+++ Vidly/Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+                return View(customer);
+
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            var customers = new List<Customer>
+            {
+                //new Customer { id = 1, Name= "Himanshu Jain"},
+                //new Customer { id = 2, Name = "Bhawna Jain"}
+            };
+            return customers;
+        }
+    }
+}
